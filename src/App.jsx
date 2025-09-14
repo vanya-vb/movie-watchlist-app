cc14edeb5c14efd85ba3c971297687b2f78b8391
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-14 16:51:18 +0300

    fix responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 		<div className="body">
 			<Header />
 
-			<main className="main">
+			<main className={`main ${movies.length > 0 && "main-filled"}`}>
 				<SearchInput
 					title={title}
 					setTitle={setTitle}
